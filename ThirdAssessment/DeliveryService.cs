0d98b870c08bfa9db79e173c2e0b759ee01ff8ce
Deepz-Deepthi <deepuramesh2003@outlook.com>
2024-10-05 12:44:13 +0530

    Add project files.

---
+++ ThirdAssessment/DeliveryService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdAssessment
+{
+    public class DeliveryService
+    {
+        public bool PlaceOrder(Product product)
+        {
+            if (product.Stock > 0)
+            {
+                return true;  //Product is in stock
+            }
+            else
+            {
+                throw new ProductOutOfStockException();
+            }
+        }
+
+    }
+}
